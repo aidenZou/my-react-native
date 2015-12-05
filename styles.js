f7f6102958685112fd0e607a786f3f721a024878
aidenzou <aidenzou@gmail.com>
2015-12-05 15:43:29 +0800

    es6

---
+++ styles.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var React = require("react-native");
+var {StyleSheet} = React;
+
+// export default class AwesomeProject extends React.Component{
+module.exports = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  }
+});
